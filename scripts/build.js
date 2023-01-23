436a9d3770fc6b15c1b6fa89f2dfe2aed6877eb3
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-01-23 10:40:45 +0100

    add a combined token lists

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,8 @@
 const lists = require("../src");
 const networkId = process.argv[2];
-const list = lists[networkId];
-if (!list) throw new Error(`List is missing: ${networkId}`);
-console.log(JSON.stringify(list, null, 2));
+
+let objectToBuild =
+  networkId === "all" ? Object.values(lists) : lists[networkId];
+if (!objectToBuild) throw new Error(`List is missing: ${networkId}`);
+
+console.log(JSON.stringify(objectToBuild, null, 2));
