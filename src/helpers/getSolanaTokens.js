015dc41052c6b91cee137525c722618f6741d822
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-01-29 11:11:42 +0100

    fix solana helper

--- src/helpers/getSolanaTokens.js
+++ src/helpers/getSolanaTokens.js
@@ -1,18 +1,18 @@
 const axios = require("axios");
-const { PublicKey, Connection } = require("@solana/web3.js");
+const { PublicKey } = require("@solana/web3.js");
 const { Metadata } = require("@metaplex-foundation/mpl-token-metadata");
 const { getMint } = require("@solana/spl-token");
 const findMetadataPda = require("./findMetadataPda");
 const getMultipleAccountsInfoSafe = require("./getMultipleAccountsInfoSafe");
 
-module.exports = async function getMultipleTmds(connection, mints) {
+module.exports = async function getSolanaTokens(connection, mints) {
   const promises = mints.map((mint) => findMetadataPda(new PublicKey(mint)));
   const metadataPdas = await Promise.all(promises);
   const metadataAccountsRes = await getMultipleAccountsInfoSafe(
     connection,
     metadataPdas
   );
-  const tmds = [];
+  const tokens = [];
   for (let i = 0; i < metadataAccountsRes.length; i++) {
     const metadataAccountRes = metadataAccountsRes[i];
     if (!metadataAccountRes) return null;
@@ -21,7 +21,7 @@ module.exports = async function getMultipleTmds(connection, mints) {
       const url = metadata.data.uri.replace(/\x00+/g, "");
       const axiosRes = await axios.get(url);
       const mintAccount = await getMint(connection, metadata.mint);
-      const tmd = {
+      const token = {
         address: metadata.mint.toString(),
         name: metadata.data.name.replace(/\x00+/g, ""),
         symbol: metadata.data.symbol.replace(/\x00+/g, ""),
@@ -30,10 +30,10 @@ module.exports = async function getMultipleTmds(connection, mints) {
         image: axiosRes.data.image || undefined,
         decimals: mintAccount.decimals,
       };
-      tmds.push(tmd);
+      tokens.push(token);
     } catch (error) {
       console.log("error", error);
     }
   }
-  return tmds.filter((tmd) => tmd !== null);
+  return tokens.filter((t) => t !== null);
 };
