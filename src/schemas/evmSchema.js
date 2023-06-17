1c165b4cd4a4528e08e762f929a4f5ceb96ae4c6
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-06-17 11:17:44 +0700

    fix

--- src/schemas/evmSchema.js
+++ src/schemas/evmSchema.js
@@ -3,6 +3,7 @@ const uniswapSchema = require("@uniswap/token-lists/src/tokenlist.schema.json");
 const evmSchema = JSON.parse(JSON.stringify(uniswapSchema));
 evmSchema.definitions.TokenInfo.properties.name.maxLength = 64;
 evmSchema.definitions.TokenInfo.properties.tags.maxItems = 20;
+evmSchema.definitions.TokenInfo.properties.symbol.maxLength = 25;
 evmSchema.definitions.TagIdentifier.maxLength = 24;
 
 const valueStringIndex =
