bd5526e30cd8b6281952a5fae76f2cfa9d90711d
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-10-31 16:04:03 +0100

    fix

---
+++ src/helpers/saveImage.js
@@ -0,0 +1,11 @@
+const { default: axios } = require("axios");
+const sharp = require("sharp");
+
+module.exports = async function saveImage(url, fileOut) {
+  const response = await axios
+    .get(url, {
+      responseType: "arraybuffer",
+    })
+    .catch((e) => null);
+  await sharp(response.data).resize(64, 64).png().toFile(fileOut);
+};
--- src/helpers/solana/getSolanaTokensFromOnChain.js
+++ src/helpers/solana/getSolanaTokensFromOnChain.js
@@ -1,3 +1,28 @@
+const { default: axios } = require("axios");
+const getSolanaOnChainTokenFromMint = require("./getSolanaOnChainTokenFromMint");
+const sleep = require("../sleep");
+const saveImage = require("../saveImage");
+
 module.exports = async function getSolanaTokensFromOnChain(alreadyFetchedSet) {
-  return [];
+  // Get SonarWatch priced mints
+  const response = await axios
+    .get("https://portfolio-api.sonar.watch/v1/portfolio/cache/")
+    .catch((e) => null);
+  if (!response) return [];
+  const data = response.data;
+  if (!data) return [];
+  const tokens = [];
+  const mints = data
+    .filter((str) => str.startsWith("tokenpricesource/solana/"))
+    .map((str) => str.replace("tokenpricesource/solana/", ""));
+  for (let i = 0; i < mints.length; i++) {
+    const mint = mints[i];
+    if (alreadyFetchedSet.has(mint)) continue;
+    const token = await getSolanaOnChainTokenFromMint(mint).catch((e) => null);
+    await sleep(500);
+    if (!token) continue;
+    await saveImage(token.logoURI, `images/solana/${token.address}.png`);
+    tokens.push(token);
+  }
+  return tokens;
 };
