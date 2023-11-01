1559e6b756a5415fbbb60b767c4a9adcd5d30791
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2023-11-01 10:43:44 +0100

    fix

--- src/helpers/solana/getSolanaTokens.js
+++ src/helpers/solana/getSolanaTokens.js
@@ -7,16 +7,16 @@ module.exports = async function getSolanaTokens(networkId, baseTokens) {
   const tokensByAddress = new Map();
 
   // Fetch from current version
-  const currentList = await axios
-    .get(
-      `https://cdn.jsdelivr.net/npm/@sonarwatch/token-lists/build/sonarwatch.${networkId}.tokenlist.json`
-    )
-    .catch(() => null);
-  if (!currentList || !currentList.data || !currentList.data.tokens)
-    throw new Error("Failed to fetch current list");
-  currentList.data.tokens.forEach((token) => {
-    tokensByAddress.set(token.address, token);
-  });
+  // const currentList = await axios
+  //   .get(
+  //     `https://cdn.jsdelivr.net/npm/@sonarwatch/token-lists/build/sonarwatch.${networkId}.tokenlist.json`
+  //   )
+  //   .catch(() => null);
+  // if (!currentList || !currentList.data || !currentList.data.tokens)
+  //   throw new Error("Failed to fetch current list");
+  // currentList.data.tokens.forEach((token) => {
+  //   tokensByAddress.set(token.address, token);
+  // });
 
   // Add from json
   const listTokens = getTokensFromList(networkId);
@@ -26,13 +26,13 @@ module.exports = async function getSolanaTokens(networkId, baseTokens) {
 
   // Add from coingecko
   let alreadyFetchedSet = new Set(Array.from(tokensByAddress.values()));
-  // const geckoTokens = await getSolanaTokensFromCoingecko(
-  //   "solana",
-  //   alreadyFetchedSet
-  // );
-  // geckoTokens.forEach((token) => {
-  //   tokensByAddress.set(token.address, token);
-  // });
+  const geckoTokens = await getSolanaTokensFromCoingecko(
+    "solana",
+    alreadyFetchedSet
+  );
+  geckoTokens.forEach((token) => {
+    tokensByAddress.set(token.address, token);
+  });
 
   // Add from on chain metadata
   alreadyFetchedSet = new Set(Array.from(tokensByAddress.values()));
