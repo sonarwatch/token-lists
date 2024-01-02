f8a1e7579225636ce8b3b546a07a352dc658b1b8
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-01-02 19:57:40 +0100

    fix

--- src/helpers/formatToken.js
+++ src/helpers/formatToken.js
@@ -1,4 +1,4 @@
-module.exports = async function formatToken(token) {
+module.exports = function formatToken(token) {
   const nToken = { ...token };
   nToken.name = token.name.substring(0, 64).trim();
   nToken.symbol = token.symbol.trim().replaceAll(" ", "").substring(0, 24);
