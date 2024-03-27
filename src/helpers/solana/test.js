fd80da43ac490c53cf1cc0c94b7fa538f07e53c3
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-03-27 12:14:02 +0100

    convert img

---
+++ src/helpers/solana/test.js
@@ -0,0 +1,22 @@
+const { default: axios } = require("axios");
+const sharp = require("sharp");
+const fs = require("fs");
+
+const folder = "./images/solana/";
+const pngFiles = [];
+fs.readdirSync(folder).forEach((file) => {
+  if (file.endsWith(".png")) pngFiles.push(file);
+});
+
+async function main() {
+  for (let i = 0; i < 10; i++) {
+    const pngFile = pngFiles[i];
+    await sharp(folder + pngFile)
+      .resize(64, 64)
+      .webp()
+      .toFile(folder + pngFile.slice(0, -3) + "webp");
+    fs.unlinkSync(folder + pngFile);
+  }
+}
+
+main();
