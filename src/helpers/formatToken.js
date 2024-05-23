01b2b1442a8bed7a03f4a6c6915f32237596298f
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-05-23 11:39:18 +0300

    fix formatToken

--- src/helpers/formatToken.js
+++ src/helpers/formatToken.js
@@ -1,6 +1,10 @@
 module.exports = function formatToken(token) {
   const nToken = { ...token };
-  nToken.name = token.name.normalize("NFKC").trim().substring(0, 64);
+  nToken.name = token.name
+    .normalize("NFKC")
+    .replace("\\", "")
+    .trim()
+    .substring(0, 64);
   nToken.symbol = token.symbol
     .replace(/[^\x20-\x7F]/g, "")
     .trim()
