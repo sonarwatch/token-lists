491631add67988d1d1bb8ad072e7d49473bad427
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-07-31 16:24:17 +0200

    fix

--- src/helpers/solana/getSolanaTokens.js
+++ src/helpers/solana/getSolanaTokens.js
@@ -2,11 +2,11 @@ const getTokensFromCurrentList = require("../getTokensFromCurrentList");
 const getTokensFromList = require("../getTokensFromList");
 const getSolanaTokensFromJup = require("./getSolanaTokensFromJup");
 
-module.exports = async function getSolanaTokens(networkId) {
+module.exports = async function getSolanaTokens() {
   const tokensByAddress = new Map();
 
   // Fetch from current version
-  const currentTokens = await getTokensFromCurrentList(networkId);
+  const currentTokens = await getTokensFromCurrentList("solana");
   const currentTokensSet = new Set();
   currentTokens.forEach((token) => {
     currentTokensSet.add(token.address);
@@ -19,7 +19,7 @@ module.exports = async function getSolanaTokens(networkId) {
   });
 
   // Add from json
-  const listTokens = getTokensFromList(networkId);
+  const listTokens = getTokensFromList("solana");
   listTokens.forEach((token) => {
     tokensByAddress.set(token.address, token);
   });
