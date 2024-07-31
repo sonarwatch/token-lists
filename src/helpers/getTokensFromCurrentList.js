85f3b0dfa9d77619a359084f02eca7a5bb537e71
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-07-31 16:45:47 +0200

    fix getTokensFromCurrentList

--- src/helpers/getTokensFromCurrentList.js
+++ src/helpers/getTokensFromCurrentList.js
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 module.exports = async function getTokensFromCurrentList(networkId) {
   const currentList = await axios
     .get(
-      `https://github.com/sonarwatch/token-lists/releases/latest/download/sonarwatch.${network.id}.tokenlist.json`
+      `https://github.com/sonarwatch/token-lists/releases/latest/download/sonarwatch.${networkId}.tokenlist.json`
     )
     .catch(() => null);
   if (!currentList || !currentList.data || !currentList.data.tokens)
