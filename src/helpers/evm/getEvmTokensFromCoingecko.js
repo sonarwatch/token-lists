f50fb5322e7cec68b8611202ad4b0648197e0a99
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-08-16 08:44:46 +0800

    fix

--- src/helpers/evm/getEvmTokensFromCoingecko.js
+++ src/helpers/evm/getEvmTokensFromCoingecko.js
@@ -41,7 +41,7 @@ module.exports = async function getEvmTokensFromCoingecko(
       continue;
     }
 
-    if (currentTokensMap.get(address) && Math.random() > 0.001) {
+    if (currentTokensMap.get(address) && Math.random() > 0.000001) {
       tokensByAddress.set(address, currentTokensMap.get(address));
       continue;
     }
