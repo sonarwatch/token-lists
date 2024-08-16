dae06b492da4c5e8bd0f1fca9fc1d0bd44d1b983
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-08-16 08:06:05 +0800

    fix

--- src/helpers/formatTokenAddress.js
+++ src/helpers/formatTokenAddress.js
@@ -32,7 +32,10 @@ function formatTokenAddressMove(address) {
   let mainAddress = splitted.at(0).toLowerCase();
 
   if (!mainAddress.startsWith("0x")) mainAddress = `0x${tAddress}`;
-  if (!isHexString(mainAddress, 32)) {
+  if (
+    !isHexString(mainAddress, 32) &&
+    mainAddress !== "0x1::aptos_coin::AptosCoin"
+  ) {
     mainAddress = `${mainAddress.slice(0, 2)}0${mainAddress.slice(2)}`;
   }
   splitted[0] = mainAddress;
