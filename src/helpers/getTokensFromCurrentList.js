408ef37a05a8269552c3c86cf927f010754da3cc
Baurain Olivier <11538800+olivbau@users.noreply.github.com>
2024-08-21 11:34:18 +0800

    fix

--- src/helpers/getTokensFromCurrentList.js
+++ src/helpers/getTokensFromCurrentList.js
@@ -1,14 +1,17 @@
 const { default: axios } = require("axios");
 
 module.exports = async function getTokensFromCurrentList(networkId) {
-  const currentList = await axios
+  let currentList = await axios
     .get(
       `https://github.com/sonarwatch/token-lists/releases/latest/download/sonarwatch.${networkId}.tokenlist.json`,
       { timeout: 120000 }
     )
-    .catch(() => null);
-  if (!currentList || !currentList.data || !currentList.data.tokens)
-    throw new Error("Failed to fetch current list");
+    .catch((e) => {
+      throw new Error("Failed to fetch current list", { cause: e });
+    });
+
+  if (!currentList.data || !currentList.data.tokens)
+    throw new Error("Failed to fetch current list data");
 
   return currentList.data.tokens;
 };
